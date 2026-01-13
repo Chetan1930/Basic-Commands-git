886ef71a03a665630494903ee9072b91319bfab2
Chetan Chauhan <devilmafia71@gmail.com>
2026-01-13 14:20:55 +0530

    this is the second number changes in order to know more about revert commands

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("chetan Chauhan");
+
+console.log("this is version 1 ")
+
+let stuff = "everything is in series only";
+
+obj1 = {
+    a : 1,
+    b : 2, 
+    c : 3, 
+    d : 4, 
+    e : 5,
+    f : 6, 
+    g : 7, 
+
+
+} // now there there are 7 items in obj1
